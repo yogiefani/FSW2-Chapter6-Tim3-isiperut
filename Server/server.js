e47170ced616ee72c1b1e2debacbefb4c78c6885
xkylz1 <fauzansaputra741@gmail.com>
2024-11-14 02:08:38 +0800

    feat: update dependencies

--- Server/server.js
+++ Server/server.js
@@ -4,13 +4,12 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 module.exports = app;
