22f88174f26cde3bddba5cd04adf6adcde9ed3f8
xkylz1 <fauzansaputra741@gmail.com>
2024-11-14 02:24:25 +0800

    feat: import modules

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,10 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const router = require("./routes");
+const docsRouter = require("./routes/documentation.route");
+const { systemController } = require("./controllers");
+
 const app = express();
 
 app.use(cors());
@@ -11,5 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan("dev"));
+
+app.get("/api/v1/health-check", systemController.healtcheck);
+app.use("/api/v1", router);
+app.use("/api-docs", docsRouter);
+app.use(systemController.onLost);
 
 module.exports = app;
