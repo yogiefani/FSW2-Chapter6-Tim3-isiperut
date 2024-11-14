8c2440ecdeb137b034a1a2f95498c5dc248a7e59
xkylz1 <fauzansaputra741@gmail.com>
2024-11-14 13:26:43 +0800

    fix: routing method

--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -10,7 +10,7 @@ const { userController } = require("../controllers");
 router.get("", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
 router.post("", userController.createUser);
-router.put("/:id", userController.updateUser);
+router.patch("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
