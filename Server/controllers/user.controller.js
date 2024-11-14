449e792d3873051ad224e87eda99d3276ac5d0ee
xkylz1 <fauzansaputra741@gmail.com>
2024-11-14 12:35:07 +0800

    feat: add pagination on user controller

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -43,12 +43,32 @@ const createUser = async (req, res) => {
 // Get all users
 const getAllUsers = async (req, res) => {
   try {
-    const allUsers = await users.findAll();
+    // Get page and limit from query, set default values if not provided
+    const page = parseInt(req.query.page) || 1; // Default to page 1
+    const limit = parseInt(req.query.limit) || 10; // Default to 10 users per page
+    const offset = (page - 1) * limit;
+
+    // Get total count of users
+    const totalUsers = await users.count();
+    const totalPages = Math.ceil(totalUsers / limit);
+
+    // Fetch users with pagination
+    const allUsers = await users.findAll({
+      limit: limit,
+      offset: offset,
+    });
+
     res.status(200).json({
       status: "Success",
       message: "Success to get users",
       isSuccess: true,
       data: allUsers,
+      pagination: {
+        totalUsers,
+        totalPages,
+        currentPage: page,
+        usersPerPage: limit,
+      },
     });
   } catch (error) {
     res.status(500).json({
