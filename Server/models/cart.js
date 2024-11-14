7e083edcc7889be6d8805972301f93fe8e4ce01c
haikalnuril <haikal.nuril23@gmail.com>
2024-11-14 13:04:27 +0700

    refactor: model and relation

--- Server/models/cart.js
+++ Server/models/cart.js
@@ -11,6 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      cart.belongsTo(models.products, {
+        foreignKey: 'productId'
+      });
+      cart.belongsTo(models.users, {
+        foreignKey: 'userId'
+      });
     }
   }
   cart.init({
