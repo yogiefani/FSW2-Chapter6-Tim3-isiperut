1190a330ca0ff2fdb8b6b678eeaac1923a062b30
xkylz1 <fauzansaputra741@gmail.com>
2024-11-16 07:23:45 +0800

    feat: add log out button

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 const Navbar = () => {
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    window.location.href = "/login";
+  };
   return (
-    <ul className="bg-red-700">
-      <li>Ini Navbar</li>
-      <li>Home</li>
-      <li>Product</li>
-    </ul>
+    <>
+      <ul className="bg-red-700">
+        <li>Ini Navbar</li>
+        <li>Home</li>
+        <li>Product</li>
+      </ul>
+      <button className="btn btn-secondary ml-5" onClick={logoutHandler}>
+        Logout
+      </button>
+    </>
   );
 };
 export default Navbar;
