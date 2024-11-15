a8e52cf24879999ce92a7b99fa97d15b1c595244
xkylz1 <fauzansaputra741@gmail.com>
2024-11-16 05:00:55 +0800

    feat: implement axios for login

---
+++ Client/src/api/axiosInstance.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+  timeout: 10000,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+axios.interceptors.response.use();
+
+export default axiosInstance;
