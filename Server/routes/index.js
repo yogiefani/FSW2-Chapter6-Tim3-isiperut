c61a71c5e402f52c26d436a7a27c4649fd01358f
xkylz1 <fauzansaputra741@gmail.com>
2024-11-16 04:14:03 +0800

    fix: redundant route

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -4,11 +4,8 @@ const User = require("./user.route");
 const Auth = require("./auth.route");
 const Product = require("./product.route");
 
-router.use("/users", User);
 router.use("/auth", Auth);
-
-
 router.use("/users", User);
-router.use("/products", Product)
+router.use("/products", Product);
 
 module.exports = router;
