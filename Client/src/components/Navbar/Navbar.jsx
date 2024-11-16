995895e3be1bb87052954bad65bf19c226b91645
Zainal <zainalfadliansyah@gmail.com>
2024-11-17 06:15:13 +0700

    refactor: adjust layout of navbar components

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -31,25 +31,49 @@ const Navbar = () => {
   const user = getusersData();
 
   return (
-    <div className="navbar bg-[#C03822] px-20 flex items-center justify-between">
-      <div className="navbar-start flex items-center gap-4">
-        <NavLink to="/" className="flex items-center">
+    <div className="navbar bg-[#C03822] px-20">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <NavbarList />
+          </ul>
+        </div>
+        <NavLink to="/" className="btn btn-ghost text-xl hidden lg:flex">
           <img
             src="../../../public/images/isi perut.png"
             alt="Logo"
-            className="w-10 h-10"
+            width="40px"
           />
-          <p className="font-bold text-orange-400 ml-2">ISI PERUT</p>
+          <p className="font-bold text-orange-400">ISI PERUT</p>
         </NavLink>
       </div>
 
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-white flex items-center gap-6">
+        <ul className="menu menu-horizontal px-1">
           <NavbarList />
         </ul>
       </div>
 
-      <div className="navbar-end flex items-center gap-4">
+      <div className="navbar-end">
         <div className="dropdown dropdown-end">
           <div
             tabIndex={0}
