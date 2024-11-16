c5ef532b4702b7c376e1627c9ac5fc6bc238b2e1
xkylz1 <fauzansaputra741@gmail.com>
2024-11-17 05:21:33 +0800

    fix: conten on product tab to exclude ative user

--- Client/src/pages/admin/AdminDashboardView.jsx
+++ Client/src/pages/admin/AdminDashboardView.jsx
@@ -6,49 +6,53 @@ import { checkAccess } from "../../middlewares/Auth";
 import CardProduct from "../../components/Card/CardProduct";
 
 export const loader = async () => {
-    //middleware buat check yang login role admin apa bukan
-    const access = await checkAccess();
+  //middleware buat check yang login role admin apa bukan
+  const access = await checkAccess();
 
-    if (!access) {
-        return redirect("/not-found");
-    }
+  if (!access) {
+    return redirect("/not-found");
+  }
 
-    const response = await axiosInstance.get("/users");
-    const users = response.data.data;
+  const response = await axiosInstance.get("/users");
+  const users = response.data.data;
 
-    const productsData = await axiosInstance.get("/products");
-    const products = productsData.data.data;
+  const productsData = await axiosInstance.get("/products");
+  const products = productsData.data.data;
 
-    return { users, products };
+  return { users, products };
 };
 
 function AdminDashboard() {
-    const { users, products } = useLoaderData();
-    return (
-        <>
-            <NavbarForAdmin />
-            <div className="m-5 mt-8">
-                <h1 className="text-4xl font-semibold text-left">Users</h1>
-            </div>
-            
-            <div className="m-5 mt-20">
-                <h1 className="text-4xl font-semibold text-left">Our Active Users</h1>
-            </div>
-            <div className="m-5 grid md:grid-cols-2 lg:grid-cols-3 gap-10">
-                {users.slice(1).map((user) => (
-                    <CardUser
-                        key={user.id}
-                        id={user.id}
-                        image={user.photoProfile? user.photoProfile : "https://ik.imagekit.io/9h1gnwzay/default-image.jpg?updatedAt=1728904313272"}
-                        name={user.name}
-                        email={user.email}
-                        phone={user.phone}
-                        address={user.address}
-                        role={user.role}
-                    />
-                ))}
-            </div>
-        </>
-    );
+  const { users } = useLoaderData();
+  return (
+    <>
+      <NavbarForAdmin />
+      <div className="m-5 mt-8">
+        <h1 className="text-4xl font-semibold text-left">Users</h1>
+      </div>
+
+      <div className="m-5 mt-20">
+        <h1 className="text-4xl font-semibold text-left">Our Active Users</h1>
+      </div>
+      <div className="m-5 grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {users.slice(1).map((user) => (
+          <CardUser
+            key={user.id}
+            id={user.id}
+            image={
+              user.photoProfile
+                ? user.photoProfile
+                : "https://ik.imagekit.io/9h1gnwzay/default-image.jpg?updatedAt=1728904313272"
+            }
+            name={user.name}
+            email={user.email}
+            phone={user.phone}
+            address={user.address}
+            role={user.role}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 export default AdminDashboard;
