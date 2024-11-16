6ea2744e3dd3f3691c534172c37f6426d2188c0e
xkylz1 <fauzansaputra741@gmail.com>
2024-11-17 05:11:21 +0800

    fix: delete notes and update bug

--- Client/src/components/Card/CardProduct.jsx
+++ Client/src/components/Card/CardProduct.jsx
@@ -5,11 +5,16 @@ import axiosInstance from "../../api/axiosInstance";
 import { toast } from "react-toastify";
 
 const CardProduct = (props) => {
-  const { image, name, description, price, stock, category, id } = props;
-  const {revalidate} = useRevalidator();
-//   console.log(props.id);
+  const { image, name, description, price, stock, category, id, deletedAt } =
+    props;
+  const { revalidate } = useRevalidator();
+
   return (
-    <div className="card bg-base-200 w-96 shadow-xl">
+    <div
+      className={`card bg-base-200 w-96 shadow-xl ${
+        deletedAt ? "opacity-70" : ""
+      }`}
+    >
       <figure>
         <img
           className="max-h-[200px] min-w-[300px] scale-125"
@@ -21,6 +26,7 @@ const CardProduct = (props) => {
         <h2 className="card-title">
           {name}
           <div className="badge badge-secondary">{category}</div>
+          {deletedAt && <div className="badge badge-error">Deleted</div>}
         </h2>
         <div className="text-primary text-left">
           ${" "}
@@ -34,22 +40,30 @@ const CardProduct = (props) => {
         <div className="font-light text-left">Stock: {stock}</div>
         <p className="text-left">{description}</p>
         <div className="card-actions justify-end items-end gap-4">
-          <Link
-            className="btn btn-accent"
-            to={`/admin-dashboard/product/${id}/edit`}
-          >
-            <BsPencilSquare />
-          </Link>
-          <button
-            className="btn btn-primary"
-            onClick={async () => {
-              await axiosInstance.delete(`/products/${id}`);
-              toast.info("Product deleted successfully");
-              revalidate(); // Refresh data after successful deletion
-            }}
-          >
-            <ImBin />
-          </button>
+          {!deletedAt && (
+            <>
+              <Link
+                className="btn btn-accent"
+                to={`/admin-dashboard/product/${id}/edit`}
+              >
+                <BsPencilSquare />
+              </Link>
+              <button
+                className="btn btn-primary"
+                onClick={async () => {
+                  try {
+                    await axiosInstance.delete(`/products/${id}`);
+                    toast.info("Product deleted successfully");
+                    revalidate(); // Refresh data after successful deletion
+                  } catch (error) {
+                    toast.error("Failed to delete product");
+                  }
+                }}
+              >
+                <ImBin />
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
