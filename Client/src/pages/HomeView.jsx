832a86a37b72c3833cf037cb1b88be9b0b14828e
AbdiPranawa <asaturaardana@gmail.com>
2024-11-17 23:39:18 +0700

    Add: product data fetching to HomeView

--- Client/src/pages/HomeView.jsx
+++ Client/src/pages/HomeView.jsx
@@ -1,9 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import axiosInstance from "../api/axiosInstance";
+import CardProduct from "../components/Card/CardProduct";
 import { useEffect, useState } from "react";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import NavbarForAdmin from "../components/Navbar/NavbarForAdmin";
 
+export const loader = async () => {
+  const response = await axiosInstance.get("/users");
+  const users = response.data.data;
+
+  const productsData = await axiosInstance.get("/products/all");
+  const products = productsData.data.data;
+
+  return { users, products };
+};
+
 function HomeView() {
+  const { products } = useLoaderData();
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
@@ -26,7 +40,14 @@ function HomeView() {
   return (
     <>
       {isAdmin ? <NavbarForAdmin /> : <Navbar />}
-      <h1>Home</h1>
+      <div className="m-5 mt-8">
+        <h1 className="text-4xl font-semibold text-left">Product On Sales</h1>
+      </div>
+      <div className="m-5 grid md:grid-cols-3 lg:grid-cols-4 gap-y-10">
+        {products.map((product) => (
+          <CardProduct key={product.id} id={product.id} image={product.image} name={product.name} description={product.desc} price={product.price} stock={product.stock} category={product.category} deletedAt={product.deletedAt} />
+        ))}
+      </div>
       <Footer />
     </>
   );
