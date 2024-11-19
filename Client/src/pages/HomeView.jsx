66cd192e6b2aca3799d590848247a4e8afb8a15d
xkylz1 <fauzansaputra741@gmail.com>
2024-11-19 19:50:36 +0800

    feat: update home page content to exclude product list

--- Client/src/pages/HomeView.jsx
+++ Client/src/pages/HomeView.jsx
@@ -61,11 +61,11 @@ function HomeView() {
       <div className="m-5 mt-8">
         <h1 className="text-4xl font-semibold text-left">Product On Sales</h1>
       </div>
-      <div className="m-5 grid md:grid-cols-3 lg:grid-cols-4 gap-y-10">
+      {/* <div className="m-5 grid md:grid-cols-3 lg:grid-cols-4 gap-y-10">
         {products.map((product) => (
           <CardProduct key={product.id} id={product.id} image={product.image} name={product.name} description={product.desc} price={product.price} stock={product.stock} category={product.category} deletedAt={product.deletedAt} />
         ))}
-      </div>
+      </div> */}
       <Footer />
     </>
   );
